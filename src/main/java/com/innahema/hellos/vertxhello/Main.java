ec3ad57122db609fa1a406e049693b1bda846ce1
Bogdan Mart <mart.bogdan@gmail.com>
2016-03-07 22:10:30 +0200

    first ruing Verticle: serving static files in  src/main/webapp/

--- src/main/java/com/innahema/hellos/vertxhello/Main.java
+++ src/main/java/com/innahema/hellos/vertxhello/Main.java
@@ -1,5 +1,7 @@
 package com.innahema.hellos.vertxhello;
 
+import io.vertx.core.Vertx;
+
 /**
  * Created by Bogdan Mart on 07.03.2016.
  */
@@ -7,6 +9,12 @@ public class Main
 {
     public static void main(String[] args)
     {
+        //Vertx vertx = Vertx.vertx(new VertxOptions().setWorkerPoolSize(40));
+        Vertx vertx = Vertx.vertx();
+
+        Server server = new Server();
+        server.init(vertx,vertx.getOrCreateContext());
+        server.start();
 
     }
 }

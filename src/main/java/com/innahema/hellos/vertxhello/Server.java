2cde54dd5a37ff0d87b8ee2487784b69e23ccefd
Bogdan Mart <mart.bogdan@gmail.com>
2016-03-26 19:57:50 +0200

    added jade templates

--- src/main/java/com/innahema/hellos/vertxhello/Server.java
+++ src/main/java/com/innahema/hellos/vertxhello/Server.java
@@ -1,9 +1,11 @@
 package com.innahema.hellos.vertxhello;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.TemplateHandler;
 import io.vertx.ext.web.templ.JadeTemplateEngine;
 import io.vertx.ext.web.templ.TemplateEngine;
@@ -13,38 +15,64 @@ import io.vertx.ext.web.templ.TemplateEngine;
  */
 public class Server extends AbstractVerticle
 {
+    TemplateEngine engine = JadeTemplateEngine.create();
     public void start() {
         HttpServer server = getVertx().createHttpServer();
 
-        TemplateEngine engine = JadeTemplateEngine.create();
-        TemplateHandler handler = TemplateHandler.create(engine);
 
 
         Router router = Router.router(vertx);
 
+        router.get("/hello/:name").handler(ctx->{
+
+            ctx.put("name", ctx.request().getParam("name"));
+            renderTemplate(ctx, "templates/hello.jade");
+        });
+        router.get("/hello").handler(ctx->{
+
+            ctx.put("name", "Default name");
+            renderTemplate(ctx, "templates/hello.jade");
+        });
+
         router.get("/").handler(routingContext -> {
             routingContext.reroute("/index.html");
         });
-        router.route().handler(routingContext -> {
+        router.get().handler(routingContext -> {
 
-            String file = routingContext.request().path();
-            String filename = "src/main/webapp/" + file;
+                String file = routingContext.request().path();
+                String filename = "src/main/webapp/" + file;
 
-            getVertx().fileSystem().exists(
-                    filename,
-                    exists -> {
-                        HttpServerResponse response = routingContext.response();
-
-                        if(exists.succeeded()&&exists.result())
-                            response.sendFile(filename);
-                        else {
-                            response.setStatusCode(404);
-                            response.end("404. File not found!");
+                getVertx().fileSystem().exists(
+                        filename,
+                        exists -> {
+                            if(exists.succeeded()&&exists.result())
+                                routingContext.response().sendFile(filename);
+                            else
+                                routingContext.next();
                         }
-                    }
-            );
-        });
+                );
+            });
+
+        router.get().handler(routingContext->{
+                HttpServerResponse response = routingContext.response();
+
+                response.setStatusCode(404);
+                response.end("404. File not found!");
+            });
 
         server.requestHandler(router::accept).listen(8080);
+    }
+
+    private void renderTemplate(RoutingContext ctx, String templateFileName)
+    {
+        engine.render(ctx, templateFileName, res->{
+            if (res.succeeded()) {
+                ctx.response()
+                        .putHeader(HttpHeaders.CONTENT_TYPE, TemplateHandler.DEFAULT_CONTENT_TYPE)
+                        .end(res.result());
+            } else {
+                ctx.fail(res.cause());
+            }
+        });
     }
 }

a4037a6225f0d826c644984bfe082e4f44fa9c88
Bogdan Mart <mart.bogdan@gmail.com>
2016-03-26 18:55:12 +0200

    Added routing

--- src/main/java/com/innahema/hellos/vertxhello/Main.java
+++ src/main/java/com/innahema/hellos/vertxhello/Main.java
@@ -1,6 +1,7 @@
 package com.innahema.hellos.vertxhello;
 
 import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
 
 /**
  * Created by Bogdan Mart on 07.03.2016.
@@ -9,8 +10,8 @@ public class Main
 {
     public static void main(String[] args)
     {
-        //Vertx vertx = Vertx.vertx(new VertxOptions().setWorkerPoolSize(40));
-        Vertx vertx = Vertx.vertx();
+        Vertx vertx = Vertx.vertx(new VertxOptions().setWorkerPoolSize(4));
+        //Vertx vertx = Vertx.vertx();
 
         Server server = new Server();
         server.init(vertx,vertx.getOrCreateContext());
--- src/main/java/com/innahema/hellos/vertxhello/Server.java
+++ src/main/java/com/innahema/hellos/vertxhello/Server.java
@@ -1,6 +1,12 @@
 package com.innahema.hellos.vertxhello;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.TemplateHandler;
+import io.vertx.ext.web.templ.JadeTemplateEngine;
+import io.vertx.ext.web.templ.TemplateEngine;
 
 /**
  * Created by Bogdan Mart on 07.03.2016.
@@ -8,11 +14,27 @@ import io.vertx.core.AbstractVerticle;
 public class Server extends AbstractVerticle
 {
     public void start() {
-        getVertx().createHttpServer().requestHandler(req -> {
-            String file = req.path().equals("/") ? "index.html" : req.path();
-//                req.response().setStatusCode(200);
-//                req.response().end();
-            req.response().sendFile("src/main/webapp/" + file);
-        }).listen(8080);
+
+        HttpServer server = getVertx().createHttpServer();
+
+
+
+        TemplateEngine engine = JadeTemplateEngine.create();
+        TemplateHandler handler = TemplateHandler.create(engine);
+
+
+        Router router = Router.router(vertx);
+
+        router.get("/").handler(routingContext -> {
+            routingContext.reroute("/index.html");
+        });
+        router.route().handler(routingContext -> {
+
+            String file = routingContext.request().path();
+            routingContext.response().sendFile("src/main/webapp/" + file);
+
+        });
+
+        server.requestHandler(router::accept).listen(8080);
     }
 }
